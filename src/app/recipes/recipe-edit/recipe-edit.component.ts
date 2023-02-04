d020817497f4d357c589198541390d56dea3d6ed
SaJid96 <sajidhussain98@gmail.com>
2023-02-05 01:15:37 +0530

    Recipe edit form completed

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute,Params } from '@angular/router';
-
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +10,8 @@ import { ActivatedRoute,Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute) { }
-
+  constructor(private route:ActivatedRoute,private recipeService:RecipeService) { }
+  recipeForm:FormGroup
   id:number;
   editmode=false
   ngOnInit(): void {
@@ -25,6 +26,25 @@ export class RecipeEditComponent implements OnInit {
           
         })
   
+  }
+
+  private initForm(){
+    let recipeName='';
+    let recipeImagePath='';
+    let recipeDescription='';
+
+    if(this.editmode){
+      const recipe=this.recipeService.getRecipe(this.id);
+      recipeName=recipe.name;
+      recipeImagePath=recipe.imagePath;
+      recipeDescription=recipe.description;
+    }
+this.recipeForm=new FormGroup({
+  'name':new FormControl(recipeName),
+  'imagePath':new FormControl(recipeImagePath),
+  'description':new FormControl(recipeDescription)
+})
+
   }
 
 }
