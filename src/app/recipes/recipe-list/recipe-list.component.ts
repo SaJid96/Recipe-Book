5ff12d86beeb8393e08fd0fa8761203c07536531
SaJid96 <sajidhussain98@gmail.com>
2023-02-07 19:10:25 +0530

    Deleting Ingredients and Some Finishing Touches

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
 
 import { Recipe } from '../recipe.model';
-
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit,OnDestroy {
   recipes: Recipe[]
+  subScription:Subscription
 
   constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
 
-    this.recipeService.recipeChanged.subscribe((recipe:Recipe[])=>{
+   this.subScription= this.recipeService.recipeChanged.subscribe((recipe:Recipe[])=>{
       this.recipes=recipe
     })
 
   this.recipes=this.recipeService.getRecipes()
   }
 
-
+ngOnDestroy(): void {
+  this.subScription.unsubscribe()
+}
 }
