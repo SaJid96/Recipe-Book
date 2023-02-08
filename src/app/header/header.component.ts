86dc10581c07dcd0b416cef1688809bd79fcaf8a
SaJid96 <sajidhussain98@gmail.com>
2023-02-08 12:53:35 +0530

    storing recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../services/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls:['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private data: DataStorageService) {}
 
+  saveResponse() {
+    this.data.storeRecipes();
+  }
+
+  fetchResponse() {
+    this.data.fetchRecipes();
+  }
 }
