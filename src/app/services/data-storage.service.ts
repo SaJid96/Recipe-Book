9e03d43a986dfbf5b7b460b48fee748a39d03c56
SaJid96 <sajidhussain98@gmail.com>
2023-02-08 13:12:49 +0530

    transforming Response Data

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from './recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -19,7 +22,19 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http
-      .get('https://recipe-book-f0812-default-rtdb.firebaseio.com/recipes.json')
-      .subscribe((response) => console.log(response));
+      .get<Recipe[]>(
+        'https://recipe-book-f0812-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((response) => this.recipeService.setRecipes(response));
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -11,20 +11,21 @@ export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
   constructor(private slService: ShoppingListService) {}
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is simply a test',
+  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Another Test Recipe',
+  //     'This is simply a test',
+  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = []
   recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
