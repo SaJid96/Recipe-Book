3910743183dd21ce929d2cf7b34f55013deb3078
SaJid96 <sajidhussain98@gmail.com>
2023-02-12 15:30:52 +0530

    Sign up request success

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+  isLoggedinMode = true;
 
-  constructor() { }
-  isLoggedinMode=true
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSwitchMode() {
+    this.isLoggedinMode = !this.isLoggedinMode;
   }
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
 
-  onSwitchMode(){
-    this.isLoggedinMode=!this.isLoggedinMode
+    if(this.isLoggedinMode){
+
+    }
+    else{
+      this.auth.signup(email, password).subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+    }
+  
+    form.reset();
   }
-onSubmit(form:NgForm){
-console.log(form.value);
-form.reset()
-
-}
 }
